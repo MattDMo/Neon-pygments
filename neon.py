d51cf671a8fa92fded3da042ff12d0259e7429e9
MattDMo <mattdmo@pigimal.com>
2013-11-19 22:46:12 -0500

    start changing colors

--- neon.py
+++ neon.py
@@ -8,7 +8,8 @@
 
     tango.py used as template, as it included all the known
     Token types, unlike most (if not all) of the styles included in the
-    Pygments distribution.
+    Pygments distribution. However, the final version of neon.py bears
+    very little resemblance to tango.
 
     :copyright: Copyright 2013 by Matt Morrison mattdmo@mattdmo.com
     :license: BSD, see LICENSE for details.
@@ -21,7 +22,8 @@ from pygments.token import Keyword, Name, Comment, String, Error, \
 
 class NeonStyle(Style):
     """
-    Neon style inspired by the Sublime Text Neon Color Scheme.
+    Neon style inspired by the Sublime Text Neon Color Scheme -
+    https://github.com/MattDMo/Neon-color-scheme
     """
 
     # work in progress...
@@ -33,17 +35,17 @@ class NeonStyle(Style):
         # No corresponding class for the following:
         #Text:                     "", # class:  ''
         Whitespace:                "underline #f8f8f8",      # class: 'w'
-        Error:                     "#a40000 border:#ef2929", # class: 'err'
-        Other:                     "#000000",                # class 'x'
+        Error:                     "#f8f8f8 bg:#800f00", # class: 'err'
+        Other:                     "#ffffff",                # class 'x'
 
-        Comment:                   "italic #8f5902", # class: 'c'
-        Comment.Multiline:         "italic #8f5902", # class: 'cm'
-        Comment.Preproc:           "italic #8f5902", # class: 'cp'
-        Comment.Single:            "italic #8f5902", # class: 'c1'
-        Comment.Special:           "italic #8f5902", # class: 'cs'
+        Comment:                   "italic #7f817e", # class: 'c'
+        Comment.Multiline:         "italic #7f817e", # class: 'cm'
+        Comment.Preproc:           "italic #7f817e", # class: 'cp'
+        Comment.Single:            "italic #7f817e", # class: 'c1'
+        Comment.Special:           "italic #7f817e", # class: 'cs'
 
-        Keyword:                   "bold #204a87",   # class: 'k'
-        Keyword.Constant:          "bold #204a87",   # class: 'kc'
+        Keyword:                   "#0b93ff",   # class: 'k'
+        Keyword.Constant:          "italic #ff07a0",   # class: 'kc'
         Keyword.Declaration:       "bold #204a87",   # class: 'kd'
         Keyword.Namespace:         "bold #204a87",   # class: 'kn'
         Keyword.Pseudo:            "bold #204a87",   # class: 'kp'
@@ -53,33 +55,31 @@ class NeonStyle(Style):
         Operator:                  "bold #ce5c00",   # class: 'o'
         Operator.Word:             "bold #204a87",   # class: 'ow' - like keywords
 
-        Punctuation:               "bold #000000",   # class: 'p'
+        Punctuation:               "bold #ffffff",   # class: 'p'
 
         # because special names such as Name.Class, Name.Function, etc.
         # are not recognized as such later in the parsing, we choose them
         # to look the same as ordinary variables.
-        Name:                      "#000000",        # class: 'n'
+        Name:                      "#ffffff",        # class: 'n'
         Name.Attribute:            "#c4a000",        # class: 'na' - to be revised
         Name.Builtin:              "#204a87",        # class: 'nb'
         Name.Builtin.Pseudo:       "#3465a4",        # class: 'bp'
-        Name.Class:                "#000000",        # class: 'nc' - to be revised
-        Name.Constant:             "#000000",        # class: 'no' - to be revised
+        Name.Class:                "#ffffff",        # class: 'nc' - to be revised
+        Name.Constant:             "#ffffff",        # class: 'no' - to be revised
         Name.Decorator:            "bold #5c35cc",   # class: 'nd' - to be revised
         Name.Entity:               "#ce5c00",        # class: 'ni'
         Name.Exception:            "bold #cc0000",   # class: 'ne'
-        Name.Function:             "#000000",        # class: 'nf'
-        Name.Property:             "#000000",        # class: 'py'
+        Name.Function:             "#ffffff",        # class: 'nf'
+        Name.Property:             "#ffffff",        # class: 'py'
         Name.Label:                "#f57900",        # class: 'nl'
-        Name.Namespace:            "#000000",        # class: 'nn' - to be revised
-        Name.Other:                "#000000",        # class: 'nx'
+        Name.Namespace:            "#ffffff",        # class: 'nn' - to be revised
+        Name.Other:                "#ffffff",        # class: 'nx'
         Name.Tag:                  "bold #204a87",   # class: 'nt' - like a keyword
-        Name.Variable:             "#000000",        # class: 'nv' - to be revised
-        Name.Variable.Class:       "#000000",        # class: 'vc' - to be revised
-        Name.Variable.Global:      "#000000",        # class: 'vg' - to be revised
-        Name.Variable.Instance:    "#000000",        # class: 'vi' - to be revised
+        Name.Variable:             "#ffffff",        # class: 'nv' - to be revised
+        Name.Variable.Class:       "#ffffff",        # class: 'vc' - to be revised
+        Name.Variable.Global:      "#ffffff",        # class: 'vg' - to be revised
+        Name.Variable.Instance:    "#ffffff",        # class: 'vi' - to be revised
 
-        # since the tango light blue does not show up well in text, we choose
-        # a pure blue instead.
         Number:                    "bold #0000cf",   # class: 'm'
         Number.Float:              "bold #0000cf",   # class: 'mf'
         Number.Hex:                "bold #0000cf",   # class: 'mh'
@@ -87,31 +87,31 @@ class NeonStyle(Style):
         Number.Integer.Long:       "bold #0000cf",   # class: 'il'
         Number.Oct:                "bold #0000cf",   # class: 'mo'
 
-        Literal:                   "#000000",        # class: 'l'
-        Literal.Date:              "#000000",        # class: 'ld'
+        Literal:                   "#ffffff",        # class: 'l'
+        Literal.Date:              "#ffffff",        # class: 'ld'
 
         String:                    "#4e9a06",        # class: 's'
         String.Backtick:           "#4e9a06",        # class: 'sb'
         String.Char:               "#4e9a06",        # class: 'sc'
-        String.Doc:                "italic #8f5902", # class: 'sd' - like a comment
+        String.Doc:                "italic #218b97", # class: 'sd' - like a comment
         String.Double:             "#4e9a06",        # class: 's2'
         String.Escape:             "#4e9a06",        # class: 'se'
-        String.Heredoc:            "#4e9a06",        # class: 'sh'
+        String.Heredoc:            "italic #218b97",        # class: 'sh'
         String.Interpol:           "#4e9a06",        # class: 'si'
         String.Other:              "#4e9a06",        # class: 'sx'
         String.Regex:              "#4e9a06",        # class: 'sr'
         String.Single:             "#4e9a06",        # class: 's1'
         String.Symbol:             "#4e9a06",        # class: 'ss'
 
-        Generic:                   "#000000",        # class: 'g'
+        Generic:                   "#ffffff",        # class: 'g'
         Generic.Deleted:           "#a40000",        # class: 'gd'
-        Generic.Emph:              "italic #000000", # class: 'ge'
+        Generic.Emph:              "italic #ffffff", # class: 'ge'
         Generic.Error:             "#ef2929",        # class: 'gr'
         Generic.Heading:           "bold #000080",   # class: 'gh'
         Generic.Inserted:          "#00A000",        # class: 'gi'
-        Generic.Output:            "italic #000000", # class: 'go'
+        Generic.Output:            "italic #ffffff", # class: 'go'
         Generic.Prompt:            "#8f5902",        # class: 'gp'
-        Generic.Strong:            "bold #000000",   # class: 'gs'
+        Generic.Strong:            "bold #ffffff",   # class: 'gs'
         Generic.Subheading:        "bold #800080",   # class: 'gu'
         Generic.Traceback:         "bold #a40000",   # class: 'gt'
     }
